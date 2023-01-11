4b1ece6f6c242fc03950fe0a9754924473ba2d2a
Zach <98441817+VidixVici@users.noreply.github.com>
2023-01-11 10:09:11 -0600

    Update vxvPassGen.py

--- vxvPassGen.py
+++ vxvPassGen.py
@@ -1,28 +1,3 @@
-# developed by Zach - Vidi x Vici
-# last updated 19 April 2022
-# For any optimizations or advise on improvement go to VidixVici.github.io
-# and fill out the form to contact me
-#
-# BEFORE USE
-# 
-# How to install On Windows
-# C:\Users\USER NAME\AppData\Local\Programs\Python\Python310
-# install get-pip.py file here
-# do this by cd'ing to correct folder path and using
-# python get-pip.py
-# install other imports
-# python -m pip install tk
-# python -m pip install pyperclip
-# create desktop shortcut
-# change target field to line below
-# drag to windows task bar
-#
-# ALSO! SCROLL THROUGH CODE AND UPDATE PASSWORD KEY AND FOLDER DIRECTORIES
-# LINES THAT NEED TO BE UPDATED BELOW
-#
-# 270, 282, 
-# 303, 304, 306, 325, 344, 345, 347, 366, 385, 386, 388, 407, 426, 427, 429, 448
-#
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import tkinter as tk 
@@ -172,22 +147,6 @@ class Window(Frame):
         extraInfoEntry.delete('1.0','20.0')
 
         titleEntry.insert(0, title)
-
-        self.decryptEmail()
-        email = encryptedEmail
-        emailEntry.insert(0, email)
-
-        self.decryptUsername()
-        username = encryptedUser
-        nameEntry.insert(0, username)
-
-        self.decryptPass()
-        Password = encryptedPass
-        passwordEntry.insert(0, Password)
-
-        self.decryptExtra()
-        extraInfo = encryptedExtra
-        extraInfoEntry.insert('1.0', extraInfo)
         
     # function for exit button
     
@@ -216,11 +175,6 @@ class Window(Frame):
         email = emailEntry.get()
         Password = passwordEntry.get()
         extraInfo = extraInfoEntry.get('1.0', '20.0')
-        self.rsaBackIn()
-        self.encryptPass()
-        self.encryptEmail()
-        self.encryptUsername()
-        self.encryptExtra()
 
     # copies username to clipboard
 
@@ -266,201 +220,8 @@ class Window(Frame):
         Password = ''.join(arrP)
         pc.copy(Password)
         passwordEntry.insert(0, Password)
-    
-    def genRsa(self):
-        secret_code = "insertYours"
-        key = RSA.generate(2048)
-        encrypted_key = key.export_key(passphrase=secret_code, pkcs=8,
-                                    protection="scryptAndAES128-CBC")
+  
 
-        file_out = open("rsa_key.bin", "wb")
-        file_out.write(encrypted_key)
-        file_out.close()
-
-        #print(key.publickey().export_key())
-
-    def rsaBackIn(self):
-        secret_code = "insertYours"
-        encoded_key = open("rsa_key.bin", "rb").read()
-        key = RSA.import_key(encoded_key, passphrase=secret_code)
-
-        #print(key.publickey().export_key())
-
-    def genPubPriv(self):
-        key = RSA.generate(2048)
-        private_key = key.export_key()
-        file_out = open("private.pem", "wb")
-        file_out.write(private_key)
-        file_out.close()
-
-        public_key = key.publickey().export_key()
-        file_out = open("receiver.pem", "wb")
-        file_out.write(public_key)
-        file_out.close()
-
-    def encryptUsername(self):
-        global username
-        global title
-        if not os.path.exists('filepath' + title + '/'):
-            os.makedirs('filepath' + title + '/')
-        data = username.encode("utf-8")
-        file_out = open('filepath' + title + '/' + title + 'user' + 'encrypted_data.bin', 'wb')
-
-        recipient_key = RSA.import_key(open("receiver.pem").read())
-        session_key = get_random_bytes(16)
-
-        # Encrypt the session key with the public RSA key
-        cipher_rsa = PKCS1_OAEP.new(recipient_key)
-        enc_session_key = cipher_rsa.encrypt(session_key)
-
-        # Encrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX)
-        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-        [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-        file_out.close()
-
-    def decryptUsername(self):
-        global title
-        global encryptedUser
-        string1 = ''
-        file_in = open('filepath' + title + '/' + title + 'user' + 'encrypted_data.bin', "rb")
-
-        private_key = RSA.import_key(open("private.pem").read())
-
-        enc_session_key, nonce, tag, ciphertext = \
-        [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
-
-        # Decrypt the session key with the private RSA key
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
-
-        # Decrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-        encryptedUser = (data.decode("utf-8"))
-
-    def encryptEmail(self):
-        global email
-        global title
-        if not os.path.exists('filepath' + title + '/'):
-            os.makedirs('filepath' + title + '/')
-        data = email.encode("utf-8")
-        file_out = open('filepath' + title + '/' + title + 'email' + 'encrypted_data.bin', 'wb')
-
-        recipient_key = RSA.import_key(open("receiver.pem").read())
-        session_key = get_random_bytes(16)
-
-        # Encrypt the session key with the public RSA key
-        cipher_rsa = PKCS1_OAEP.new(recipient_key)
-        enc_session_key = cipher_rsa.encrypt(session_key)
-
-        # Encrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX)
-        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-        [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-        file_out.close()
-
-    def decryptEmail(self):
-        global title
-        global encryptedEmail
-        string1 = ''
-        file_in = open('filepath' + title + '/' + title + 'email' + 'encrypted_data.bin', "rb")
-
-        private_key = RSA.import_key(open("private.pem").read())
-
-        enc_session_key, nonce, tag, ciphertext = \
-        [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
-
-        # Decrypt the session key with the private RSA key
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
-
-        # Decrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-        encryptedEmail = (data.decode("utf-8"))
-
-    def encryptPass(self):
-        global Password
-        global title
-        if not os.path.exists('filepath' + title + '/'):
-            os.makedirs('filepath' + title + '/')
-        data = Password.encode("utf-8")
-        file_out = open('filepath' + title + '/' + title + 'pass' + 'encrypted_data.bin', 'wb')
-
-        recipient_key = RSA.import_key(open("receiver.pem").read())
-        session_key = get_random_bytes(16)
-
-        # Encrypt the session key with the public RSA key
-        cipher_rsa = PKCS1_OAEP.new(recipient_key)
-        enc_session_key = cipher_rsa.encrypt(session_key)
-
-        # Encrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX)
-        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-        [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-        file_out.close()
-
-    def decryptPass(self):
-        global title
-        global encryptedPass
-        string1 = ''
-        file_in = open('filepath' + title + '/' + title + 'pass' + 'encrypted_data.bin', "rb")
-
-        private_key = RSA.import_key(open("private.pem").read())
-
-        enc_session_key, nonce, tag, ciphertext = \
-        [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
-
-        # Decrypt the session key with the private RSA key
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
-
-        # Decrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-        encryptedPass = (data.decode("utf-8"))
-
-    def encryptExtra(self):
-        global extraInfo
-        global title
-        if not os.path.exists('filepath' + title + '/'):
-            os.makedirs('filepath' + title + '/')
-        data = extraInfo.encode("utf-8")
-        file_out = open('filepath' + title + '/' + title + 'extra' + 'encrypted_data.bin', 'wb')
-
-        recipient_key = RSA.import_key(open("receiver.pem").read())
-        session_key = get_random_bytes(16)
-
-        # Encrypt the session key with the public RSA key
-        cipher_rsa = PKCS1_OAEP.new(recipient_key)
-        enc_session_key = cipher_rsa.encrypt(session_key)
-
-        # Encrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX)
-        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-        [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-        file_out.close()
-
-    def decryptExtra(self):
-        global extraInfo
-        global encryptedExtra
-        string1 = ''
-        file_in = open('filepath' + title + '/' + title + 'extra' + 'encrypted_data.bin', "rb")
-
-        private_key = RSA.import_key(open("private.pem").read())
-
-        enc_session_key, nonce, tag, ciphertext = \
-        [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
-
-        # Decrypt the session key with the private RSA key
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
-
-        # Decrypt the data with the AES session key
-        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-        encryptedExtra = (data.decode("utf-8"))
 
 root = Tk()
 app = Window(root)
